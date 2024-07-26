48a31959967dbb3dbd28d8879cb45ee314b656db
watteja <mateyast@gmail.com>
2024-07-26 14:36:56 +0200

    Exercise 3.3: Phonebook backend step 3

--- index.js
+++ index.js
@@ -36,6 +36,16 @@ app.get("/info", (request, response) => {
   response.send(`<p>Phonebook has info for ${amount}</p><p>${currTime}</p>`);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id; // id attribute is a string
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
