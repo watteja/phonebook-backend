9fd1c9baaf5e64da7d5f3f2d0e2850424d1cf322
watteja <mateyast@gmail.com>
2024-07-26 15:18:08 +0200

    Exercise 3.6: Phonebook backend step 6

--- index.js
+++ index.js
@@ -57,7 +57,18 @@ app.delete("/api/persons/:id", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
   const body = request.body;
-  console.log("body", body);
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "you must provide name and number",
+    });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
 
   const person = {
     id: Math.floor(Math.random() * 10000),
