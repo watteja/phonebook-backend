2342bd0e86922cef02d0173880ab67789252fe47
watteja <mateyast@gmail.com>
2024-07-28 18:25:59 +0200

    Exercise 3.17: Phonebook database, step 5

--- index.js
+++ index.js
@@ -99,6 +99,21 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
 // unknown endpoint handler (must be used after all routes are registered)
 const unknownEndpoint = (_request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
